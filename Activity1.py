70ff1c3eb4d91763c587a616c34588bc177f6bc1
Sport246 <itsnihanth.cool@gmail.com>
2026-02-05 18:43:12 +0100

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,18 @@
+a = 5
+b = 0
+c = 14
+if a and b and c:
+    print("All numbers are true.")
+else:
+    print("At least 1 number is false")
+if a > 0 and b > 0:
+    print("Either the numbers are greater than 0.")
+elif b > 0 and c > 0:
+    print("Either the numbers are greater than 0")
+else:
+    print("No number is greater than 0.")
+d = 13
+e = 26
+f = 26
+print(d != e)
+print(e != f)
