61c96c23cf8e5d086281e11e8e1da070ac97cfe1
Sport246 <itsnihanth.cool@gmail.com>
2026-02-12 18:53:02 +0100

    Add files via upload

---
+++ Aktivity1.py
@@ -0,0 +1,21 @@
+x = 15
+if (type(x)) is int:
+    print("True")
+else:
+    print("False")
+
+y = 28
+if(type(y)) is not float:
+    print("True")
+else:
+    print("False")
+
+a = 10
+b= -10
+print("a >> 1 = ", a >> 1)
+print("b >> 1 =", b >> 1)
+
+c = 5
+d = -10
+print("c << 1 = ", c << 1)
+print("d << 1 = ", d << 1)
