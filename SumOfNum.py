e73b3e3ee373a4a69a26e62b97c336abaa4cd7b4
Sport246 <itsnihanth.cool@gmail.com>
2026-03-26 18:34:43 +0100

    Add files via upload

---
+++ SumOfNum.py
@@ -0,0 +1,5 @@
+numtill = int(input("Till which number will it be added?: "))
+sum = 0
+for i in range(1, numtill+1):
+    sum = sum + i
+    print(sum)
---
+++ reverse.py
@@ -0,0 +1,9 @@
+str = input("Enter a string: ")
+str1 = (" ")
+for i in str:
+    str1 = i + str1
+print(str, str1)
+
+n = int(input("Enter value of n: "))
+for i in range(n,0,-1):
+    print(i)
